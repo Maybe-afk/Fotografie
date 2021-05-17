cdf8d9250df726c9f19a5072058e11d244ab8e0e
Kuba Predki <kubapredki1@gmail..com>
2021-05-17 14:23:19 +0200

    strona ogloszenia

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -71,7 +71,7 @@ class ProductRepository extends ServiceEntityRepository
         return $this->paginator->paginate(
         $query,
         $search->page,
-        3
+        6
         );
         
 
